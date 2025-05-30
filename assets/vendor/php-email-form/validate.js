e7ebbaf1f171402496300e638edfe1df5eb6c9b4
Malaika Tariq <tariqmalaika09@gmail.com>
2025-05-30 18:22:56 +0500

    my portfolio

---
+++ assets/vendor/php-email-form/validate.js
@@ -0,0 +1,89 @@
+(function () {
+  "use strict";
+
+  let forms = document.querySelectorAll('.php-email-form');
+
+  forms.forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      let thisForm = this;
+
+      let action = thisForm.getAttribute('action');
+      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+
+      if (!action) {
+        displayError(thisForm, 'The form action property is not set!');
+        return;
+      }
+
+      thisForm.querySelector('.loading').classList.add('d-block');
+      thisForm.querySelector('.error-message').classList.remove('d-block');
+      thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+      let formData = new FormData(thisForm);
+
+      if (recaptcha) {
+        if (typeof grecaptcha !== "undefined") {
+          grecaptcha.ready(function () {
+            try {
+              grecaptcha.execute(recaptcha, { action: 'php_email_form_submit' })
+                .then(token => {
+                  formData.set('recaptcha-response', token);
+                  php_email_form_submit(thisForm, action, formData);
+                });
+            } catch (error) {
+              displayError(thisForm, error);
+            }
+          });
+        } else {
+          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!');
+        }
+      } else {
+        php_email_form_submit(thisForm, action, formData);
+      }
+    });
+  });
+
+  function php_email_form_submit(thisForm, action, formData) {
+    fetch(action, {
+      method: 'POST',
+      body: formData,
+      headers: { 'Accept': 'application/json' } // <-- important for Formspree
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();  // parse JSON instead of text
+        } else {
+          throw new Error(`${response.status} ${response.statusText} ${response.url}`);
+        }
+      })
+      .then(data => {
+        thisForm.querySelector('.loading').classList.remove('d-block');
+
+        if (data.ok) {  // check data.ok for success
+           const sentMsg = thisForm.querySelector('.sent-message');
+           sentMsg.classList.add('d-block');
+           thisForm.reset();
+           setTimeout(() => {
+              sentMsg.classList.remove('d-block');
+            }, 5000);
+
+        } else {
+          throw new Error(data.error || 'Form submission failed with no specific error message.');
+        }
+      })
+      .catch((error) => {
+        displayError(thisForm, error);
+      });
+  }
+
+  function displayError(thisForm, error) {
+    thisForm.querySelector('.loading').classList.remove('d-block');
+    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').classList.add('d-block');
+    setTimeout(() => {
+    errorMsg.classList.remove('d-block');
+  }, 5000);
+  }
+})();
